33019ba62b0efef2049485e2090a7ba95d5b853d
xdrelos <pierrejeancedric@gmail.com>
2021-04-14 10:20:16 +0200

    add stocks controller

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
+
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_publishable_token],
       secret_token: Rails.application.credentials.iex_client[:sandbox_secret_token],
