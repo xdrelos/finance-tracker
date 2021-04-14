c0ead05b0178bf93c66bcfdf214aabc4329eda75
xdrelos <pierrejeancedric@gmail.com>
2021-04-14 13:08:35 +0200

    ajax search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,23 @@
 class StocksController < ApplicationController
 
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
-
+    if params[:stock].present?
+      
+      @stock = Stock.new_lookup(params[:stock])
+      
+      if @stock
+        respond_to do |format|
+          format.js {render partial: 'users/search_result'}
+        end
+      else
+        flash[:alert] = "Please enter a valid ticker."
+        redirect_to my_portfolio_path
+      end
+      
+    else
+      flash[:alert] = "Please enter a ticker to search."
+      redirect_to my_portfolio_path
+    end
   end
 
 end
