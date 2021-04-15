6e0024dd9fe590a8d665e788d43e3b38bc117024
xdrelos <pierrejeancedric@gmail.com>
2021-04-15 09:59:55 +0200

    search

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,13 @@
 class UserStocksController < ApplicationController
+
+  def create
+    if !Stock.find_by(ticker: params[:ticker])
+      stock = Stock.new_lookup(params[:ticker])
+      
+    else
+      stock = Stock.find_by(ticker: params[:ticker]
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+  end
+
 end
